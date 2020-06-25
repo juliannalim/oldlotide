fe94ce69bd00963fd0283df65ad7eb2a9b40c0c7
Julianna <julianna.j.lim@gmail.com>
2020-06-24 21:01:45 -0400

    middle assignment

---
+++ middle.js
@@ -0,0 +1,53 @@
+
+// TEST/ASSERTION FUNCTIONS
+function eqArrays (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+    }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }  
+  return true;
+};
+
+function assertArraysEqual (arrayOne, arrayTwo) {
+  if (typeof(arrayOne) !== 'object' || typeof(arrayTwo) !== 'object') {
+    return `🛑🛑🛑 Assertion Failed: ${[arrayOne]} !== ${[arrayTwo]}`;
+  } 
+  if (arrayOne.length !== arrayTwo.length) {
+    return `🛑🛑🛑 Assertion Failed: ${[arrayOne]} !== ${[arrayTwo]}`;
+  } 
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return `🛑🛑🛑 Assertion Failed: ${[arrayOne]} !== ${[arrayTwo]}`;
+    }
+  }
+  return `✅✅✅ Assertion Passed: ${[arrayOne]} === ${[arrayTwo]}`;
+};
+
+
+// ACTUAL FUNCTION
+const middle = function(array) {
+  let middleNum = [];
+  if (array.length < 3) {
+    return middleNum;
+  }
+  // if there is an even number of arrays, there will be two arrays in the middle
+  else if (array.length %2 === 0) {
+    // array.length/2 - 1 to get one of the arrays ex. 4/2 - 1 to get 1
+    middleNum.push(array[(array.length/2)-1]);
+    // ex 4/2 will get you 2
+    middleNum.push(array[array.length / 2]);
+  } else {
+    // if it an odd number of array, it will just include middle single number
+    middleNum.push(array[(array.length - 1) / 2]);
+  }
+  return middleNum;
+}
+
+// TEST CODE
+// ...
+console.log(assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']));
+console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
