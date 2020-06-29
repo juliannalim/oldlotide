a3ff61a003f953bbc5f7e52e742a967c813d7958
Julianna <julianna.j.lim@gmail.com>
2020-06-29 16:13:09 -0400

    used mocha and chia

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
 
 // Test Code
-console.log(assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+// console.log(assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']));
+// console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+
+const assert = require('chai').assert;
+const head   = require('../middle');
+
+describe("#middle", () => {
+  it('returns ["2", "3"] for ["1", "2", "3", "4"]', () => {
+    assert.deepEqual(head(["1", "2", "3", "4"]), ["2", "3"]);
+  });
+
+  it('returns ["c"] for ["a", "b", "c", "d", "e"]', () => {
+    assert.deepEqual(head(["a", "b", "c", "d", "e"]), ["c"]);
+  });
+
+});
